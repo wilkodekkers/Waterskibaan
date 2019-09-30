e144b50c3098d14c10e2c2077a5b64d35bcf9031
Wilko Dekkers <wilko.dekkers@gmail.com>
2019-09-30 17:52:42 +0200

    Added: opdracht 1

--- Waterskibaan/Sporter.cs
+++ Waterskibaan/Sporter.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Drawing;
 
 namespace Waterskibaan
 {
     class Sporter
     {
+        private int _aantalRondenNogTeGaan;
+        private Zwemvest _zwemvest;
+        private Skies _skies;
+        private Color _kledingKleur;
+        private List<IMoves> _moves;
+
+        public int AantalRondenNogTeGaan { get => _aantalRondenNogTeGaan; set => _aantalRondenNogTeGaan = value; }
+        public Zwemvest Zwemvest { get => _zwemvest; set => _zwemvest = value; }
+        public Color KledingKleur { get => _kledingKleur; set => _kledingKleur = value; }
+        public Skies Skies { get => _skies; set => _skies = value; }
+        public List<IMoves> Moves { get => _moves; set => _moves = value; }
+
+        public Sporter(List<IMoves> moves)
+        {
+            Moves = moves;
+        }
     }
 }
