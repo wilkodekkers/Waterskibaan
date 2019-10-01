2774b4104c52c2a43358faadb40e33bc2b8d1682
Wilko Dekkers <wilko.dekkers@gmail.com>
2019-10-01 17:18:09 +0200

    Finished: 9 & 10

---
+++ Waterskibaan/Wachtrij.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    public abstract class Wachtrij
+    {
+        public virtual int MaxLengteRij { get => 5; }
+
+        private Queue<Sporter> _queue = new Queue<Sporter>();
+
+        public void SporterNeemPlaatsInRij(Sporter sporter)
+        {
+            if (_queue.Count >= MaxLengteRij) return;
+
+            _queue.Enqueue(sporter);
+        }
+
+        public List<Sporter> GetAlleSporters()
+        {
+            if (_queue.Count == 0) return new List<Sporter>();
+
+            List<Sporter> sporters = new List<Sporter>();
+
+            foreach (Sporter sporter in _queue)
+            {
+                sporters.Add(sporter);
+            }
+
+            return sporters;
+        }
+
+        public List<Sporter> SportersVerlatenRij(int aantal)
+        {
+            if (_queue.Count == 0) return new List<Sporter>();
+
+            List<Sporter> sporters = new List<Sporter>();
+
+            int amount = aantal < _queue.Count ? aantal : aantal - _queue.Count;
+
+            for (int i = 0; i < amount; i++)
+            {
+                sporters.Add(_queue.Dequeue());
+            }
+
+            return sporters;
+        }
+    }
+}
