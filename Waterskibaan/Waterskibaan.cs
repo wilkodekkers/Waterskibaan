52d064bae446422fa9464f5c0d827eb6779370ab
Wilko Dekkers <wilko.dekkers@gmail.com>
2019-10-01 09:18:37 +0200

    Finished: opdracht 4

---
+++ Waterskibaan/Waterskibaan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    class Waterskibaan
+    {
+        private Kabel _kabel = new Kabel();
+        private LijnenVoorraad _lijnen = new LijnenVoorraad();
+
+        public Waterskibaan()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                _lijnen.LijnToevoegenAanRij(new Lijn());
+            }
+        }
+
+        public void VerplaatsKabel()
+        {
+            _kabel.VerschuifLijnen();
+
+            Lijn lijn = _kabel.VerwijderLijnVanKabel();
+
+            if (lijn != null)
+            {
+                _lijnen.LijnToevoegenAanRij(lijn);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{_lijnen} | {_kabel}";
+        }
+    }
+}
